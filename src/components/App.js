e95d3d031d875535bbaed98e9055e17402b1be75
J.C <jeremiah.carlo.reyes@gmail.com>
2020-04-27 22:41:04 -0400

    Jcschanges

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import useInterval from '../hooks/use-interval.hook';
+
 import GlobalStyles from './GlobalStyles';
+import { GameContext } from './GameContext.js';
 import Home from './Home';
 import Game from './Game';
 
 function App(props) {
+  const { numCookies, setNumCookies, cookiesPerSecond } = React.useContext(
+    GameContext
+  );
+
+  useInterval(() => {
+    setNumCookies(numCookies + cookiesPerSecond);
+  }, 1000);
+
   return (
     <>
       <GlobalStyles />
